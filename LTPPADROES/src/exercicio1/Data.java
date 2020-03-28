e9d658deb83c09207232fdf802b0894609f7f274
rhayk <42190718+rhayk@users.noreply.github.com>
2020-03-27 22:20:20 -0300

    Trabalho - Representação de Relógio

---
+++ LTPPADROES/src/exercicio1/Data.java
@@ -0,0 +1,47 @@
+package exercicio1;
+
+public class Data {
+	
+	private byte dia;
+	private byte mes;
+	private short ano;
+	
+	public Data(int dia, int mes, int ano) {
+		setDia(dia);
+		setMes(mes);
+		setAno(ano);
+	}
+
+	public byte getDia() {
+		return dia;
+		
+	}
+	
+	private void setDia(int dia) {
+		
+		
+	}
+	
+	
+	public byte getMes() {
+		return mes;
+		
+	}
+
+	private void setMes(int mes) {
+		
+		
+	}
+	
+	public short getAno() {
+		return ano;
+		
+	}
+	
+	private void setAno(int ano) {
+		
+		
+	}
+	
+		
+}
